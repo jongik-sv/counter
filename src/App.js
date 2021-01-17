bb766ead8dd7fc23028bd787df4559e66f62026c
jongik-sv <yoo7032@gamil.com>
2021-01-18 00:37:48 +0900

    counter 최초 생성

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Button from "./Button";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+    // this.onClickHandler = this.onClickHandler.bind(this);
+  }
+
+  // onClickHandler(payLoad) {
+  onClickHandler = (payLoad) => {
+    // console.log("click");
+    this.setState({ number: this.state.number + payLoad });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Display value={this.state.number} />
+        {/* <button onClick={this.onClickHandler}>증가버튼</button> */}
+        {/* <button onClick={() => this.onClickHandler(1)}>증가버튼</button> */}
+        <Button clickHandler={() => this.onClickHandler(1)}>증가버튼</Button>
+        <Button clickHandler={() => this.onClickHandler(-1)}>감소버튼</Button>
+      </div>
+    );
+  }
+}
+
+const Display = ({ value }) => (
+  <div>
+    <h1>{value}</h1>
+  </div>
+);
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button = ({ children, clickHandler }) => (
+  <div>
+    <button onClick={clickHandler}>{children}</button>
+  </div>
+);
+
+export default Button;
